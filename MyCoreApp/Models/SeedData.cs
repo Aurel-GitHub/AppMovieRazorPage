c5fe6cd7104b0c1b8f42f83ec875c5917e9dc14b
aurel91000 <56591961+aurel91000@users.noreply.github.com>
2020-06-27 10:01:18 +0200

    Rating Movies

--- MyCoreApp/Models/SeedData.cs
+++ MyCoreApp/Models/SeedData.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿#define Rating
+#if Rating
+#region snippet_1 
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
 using MyCoreApp.Data;
-
-
 
 
 namespace MyCoreApp.Models
@@ -29,7 +30,8 @@ namespace MyCoreApp.Models
                         Title = "Star Wars : Episode IV A new hope",
                         ReleaseDate = DateTime.Parse("1977-10-19"),
                         Genre = "Fiction Movie",
-                        Price = 7.99M
+                        Price = 7.99M,
+                        Rating = "R"
                     },
 
                     new Movie
@@ -37,7 +39,8 @@ namespace MyCoreApp.Models
                         Title = "Star Wars : Episode V The Empire Strikes Back ",
                         ReleaseDate = DateTime.Parse("1980-8-20"),
                         Genre = "Fiction Movie",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -45,7 +48,8 @@ namespace MyCoreApp.Models
                         Title = "Star Wars: Episode VI Return of the Jedi",
                         ReleaseDate = DateTime.Parse("1983-10-19"),
                         Genre = "Fiction Movie",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -53,7 +57,8 @@ namespace MyCoreApp.Models
                         Title = "Harry Potter and the Deathly Hallows",
                         ReleaseDate = DateTime.Parse("2011-7-15"),
                         Genre = "Fantasy Movie",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "NA"
                     }
                 );
                 context.SaveChanges();
@@ -61,3 +66,5 @@ namespace MyCoreApp.Models
         }
     }
 }
+#endregion
+#endif
